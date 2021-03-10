f26b3838324da35c3a23e4bb0db20ff42632e5d6
diseo <diseo_rvt@hotmail.es>
2021-03-10 01:47:53 -0600

    Fin del bloque Variables

---
+++ 02-Variables/js/02-app.js
@@ -0,0 +1,15 @@
+
+let producto = 'Tablet';
+console.log(producto);
+
+
+producto = 'Monitor';
+console.log(producto);
+producto = 20;
+console.log(producto);
+
+let precio;
+console.log(precio);
+
+precio = 300;
+console.log(precio);
