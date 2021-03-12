3cba36bd264a75ac53437bade4c328f35fb9be1b
diseo <diseo_rvt@hotmail.es>
2021-03-12 02:26:07 -0600

    Inicio del bloque Strings

---
+++ 03-Strings/js/01-app.js
@@ -0,0 +1,8 @@
+
+const producto = 'Monitor de 20 Pulgadas'
+const producto2 = String('Monitor de 24 Pulgadas')
+
+const producto3 = new String('Monitor de 27 Pulgadas')
+
+console.log({producto, producto2, producto3});
+
---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,6 @@
+const producto = 'Monitor de 20 Pulgadas'
+
+console.log(producto)
+console.log(producto.length);
+console.log(producto.indexOf('de'));
+console.log(producto.includes('20'));
