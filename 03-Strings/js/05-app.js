e200f0f644c752a0ee9331e58fd3fdb3ecb36dc1
diseo <diseo_rvt@hotmail.es>
2021-03-15 00:22:03 -0600

    Fin del bloque Strings

---
+++ 03-Strings/js/05-app.js
@@ -0,0 +1,11 @@
+
+const producto = "Monitor de 20 Pulgadas"
+
+console.log(producto);
+console.log(producto.replace("Monitor", "Monitor curvo"));
+console.log(producto.slice(0,7));
+console.log(producto.substring(4,8));
+
+// Primer letra
+const nombre = "Ricardo"
+console.log(nombre.substring(0,1))
---
+++ 03-Strings/js/06-app.js
@@ -0,0 +1,11 @@
+const producto = "Monitor de 20 Pulgadas"
+
+const texto = 'En promoción'.repeat(3)
+
+console.log(texto);
+
+const actividad = "Estoy aprendiendo javascript Moderno"
+console.log(actividad.split(" "));
+
+const hobbies = 'leer, caminar, correr, comer'
+console.log(hobbies.split(","));
